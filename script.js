c0cc0dbb141154a4bb3cf79270b32cb01330cb77
Paweł Wolski <pawelwolski3@gmail.com>
2019-10-29 16:11:49 +0100

    add Date

---
+++ script.js
@@ -0,0 +1,12 @@
+function setDate() {
+
+  const date = new Date()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+  
+
+
+}
+
+setInterval(setDate, 1000)
