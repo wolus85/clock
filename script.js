2cc2f2e1fe7acd15b8da2f682940c71474054f33
Paweł Wolski <pawelwolski3@gmail.com>
2019-11-08 13:44:46 +0100

    finish

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const secondHand = document.querySelector('.second-hand') 
+const minuteHand = document.querySelector('.min-hand') 
+const hourHand = document.querySelector('.hour-hand') 
 function setDate() {
 
   const date = new Date()
@@ -5,8 +8,14 @@ function setDate() {
   const minute = date.getMinutes()
   const second = date.getSeconds()
   
+  const secondDegrees = ((second/60)*360) + 90
+  const minuteDegrees = ((minute/60)*360) + 90
+  const hourDegrees = ((hour/12)*360) + 90
+
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`
+  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`
 
 
-}
-
+  }
 setInterval(setDate, 1000)
